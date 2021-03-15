3c376b9f9f4201aca15219efaa1d321cfde98872
KateKurakina <58449858+KateKurakina@users.noreply.github.com>
2021-03-15 22:46:15 +0300

    init: transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,25 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Error');
+  }
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  let tArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (arr[i - 2] !== '--discard-next') {
+        tArr.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      if (i !== arr.length - 1) {
+        tArr.push(arr[i + 1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (i !== 0 && arr[i - 2] !== '--discard-next') {
+        tArr.push(arr[i - 1]);
+      }
+    } else tArr.push(arr[i]);
+  }
+  return tArr;
+}
