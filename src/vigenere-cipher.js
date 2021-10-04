179861fad3d46eb7f7b46d20d8c7d51cbef8dad1
KateKurakina <58449858+KateKurakina@users.noreply.github.com>
2021-10-05 01:01:27 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,31 +1,43 @@
 import { NotImplementedError } from '../extensions/index.js';
 
-/**
- * Implement class VigenereCipheringMachine that allows us to create
- * direct and reverse ciphering machines according to task description
- * 
- * @example
- * 
- * const directMachine = new VigenereCipheringMachine();
- * 
- * const reverseMachine = new VigenereCipheringMachine(false);
- * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
- * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
- * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
- * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
- */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if(message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    let result = '';
+    let count = 0;
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for (let i = 0; i < message.length; i++){
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) < 91) {
+        result += String.fromCharCode((message.charCodeAt(i) + key.charCodeAt(count % key.length) - 130) % 26 + 65);
+        count++;
+      }
+      else result += message[i];
+    }
+    if (this.isDirect) return result;
+    else return result.split('').reverse().join('');
+  }
+  decrypt(message, key) {
+    if(message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    let result = '';
+    let count1 = 0;
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+    for (let i = 0; i < message.length; i++){
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        result += String.fromCharCode((message.charCodeAt(i) + 26 - key.charCodeAt(count1 % key.length)) % 26 + 65);
+        count1++;
+      }
+      else result += message[i];
+    }
+    if (this.isDirect) return result;
+    else return result.split('').reverse().join('');
   }
 }
