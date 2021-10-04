377e888410973e377ee177900aaa365a41617afe
KateKurakina <58449858+KateKurakina@users.noreply.github.com>
2021-10-05 00:59:27 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,21 +1,26 @@
 import { NotImplementedError } from '../extensions/index.js';
 
-/**
- * Create a repeating string based on the given parameters
- *  
- * @param {String} str string to repeat
- * @param {Object} options options object 
- * @return {String} repeating string
- * 
- *
- * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
- *
- */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  str = String(str);
+  let res = '';
+
+  if (options.hasOwnProperty('addition')) {
+    str += String(options.addition);
+  }
+
+  if (options.hasOwnProperty('additionRepeatTimes')) {
+    for (let i = 1; i < options.additionRepeatTimes; i++) {
+      str += (options.additionSeparator || '|') + options.addition;
+    }
+  }
+
+  if (options.hasOwnProperty('repeatTimes')) {
+    for (let i = 1; i < options.repeatTimes; i++) {
+      res += str + (options.separator || '+');
+    }
+    res += str;
+  } else {
+    res = str;
+  }
+  return res;
 }
